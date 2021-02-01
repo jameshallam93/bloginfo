8a12265ff6a11acb0b14acbc85ac09f6ad92763f
James <james.hallam93@gmail.com>
2021-02-01 16:51:30 +0000

    minor changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logger = require("./utils/logger")
 const server = http.createServer(app)
 
 
-const PORT = config.PORT
+const PORT = config.PORT || 3001
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
